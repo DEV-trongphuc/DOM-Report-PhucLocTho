c78a383255e97de1e8de4cdc8735887fe60e34e7
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2025-02-13 20:32:28 +0700

    Update domvar.js

--- domvar.js
+++ domvar.js
@@ -17,13 +17,20 @@ const goalMapping = {
     "PROFILE_VISIT",
     "LADING_PAGE_VIEWS",
   ],
-    Pagelike: ["PAGE_LIKES"],
-
+  Pagelike: ["PAGE_LIKES"],
 };
-
-
+const NAME = "Phúc Lộc Thọ";
+const LOGO_ACCOUNT = "./DOM-img/logo_plt.jpg";
 const accessToken =
   "EAAQwS9m6tIABO8ZCZCvO4TtPBXcbilAYn3nwZCZB739B8GtMfy2V2uJmgjHMtvsdKS6XMl7YiBuXqg3BxTdh37H7Vv5qYcsZA7IqVYMLqHX3FhQdxD8fSguISa0sDg1INzOfVtUCt8OoNqh0j6PXvu50rZCgMerGZAJ7NAYgLYuTsPw8NvdOEdF5kRX9C0ctu1ka7CS6VcbbXosWnMM"; // Token của bạn
 const adAccountId = "1177075676820030"; // PHÚC LỘC THỌ
 
 let oodo_view = false;
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#dom_account_name").textContent = NAME;
+  document.querySelector("#dom_account_name_r").textContent = NAME;
+  document.querySelector("#dom_account_id").textContent = adAccountId;
+  document.querySelector("#dom_account_src").src = LOGO_ACCOUNT;
+  document.querySelector("#dom_account_src_r").src = LOGO_ACCOUNT;
+  document.title = `${NAME} - Meta Ads Report by DOM Marketing`;
+});
