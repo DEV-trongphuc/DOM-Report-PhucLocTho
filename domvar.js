d7eeda492397882ed9f7c69ccccac54ecb20f6db
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2025-02-21 11:00:16 +0700

    Update domvar.js

--- domvar.js
+++ domvar.js
@@ -1,4 +1,4 @@
-const quick_filter = [
+const quickFilter = [
   "Lead Form",
   "Awareness",
   "Engagement",
@@ -6,31 +6,7 @@ const quick_filter = [
   "Traffic",
   "Pagelike",
 ];
-const goalMapping = {
-  "Lead Form": ["LEAD_GENERATION", "QUALITY_LEAD"],
-  Awareness: ["REACH", "AD_RECALL_LIFT", "IMPRESSIONS"],
-  Engagement: ["POST_ENGAGEMENT", "THRUPLAY", "EVENT_RESPONSES"],
-  Message: ["REPLIES"],
-  Traffic: [
-    "OFFSITE_CONVERSIONS",
-    "LINK_CLICKS",
-    "PROFILE_VISIT",
-    "LADING_PAGE_VIEWS",
-  ],
-  Pagelike: ["PAGE_LIKES"],
-};
-const NAME = "Phúc Lộc Thọ";
-const LOGO_ACCOUNT = "./DOM-img/logo_plt.jpg";
-const accessToken =
-  "EAAQwS9m6tIABO8ZCZCvO4TtPBXcbilAYn3nwZCZB739B8GtMfy2V2uJmgjHMtvsdKS6XMl7YiBuXqg3BxTdh37H7Vv5qYcsZA7IqVYMLqHX3FhQdxD8fSguISa0sDg1INzOfVtUCt8OoNqh0j6PXvu50rZCgMerGZAJ7NAYgLYuTsPw8NvdOEdF5kRX9C0ctu1ka7CS6VcbbXosWnMM"; // Token của bạn
+let isBrand = false;
+const accName = "Phúc Lộc Thọ";
+const accAvatar = "./DOM-img/logo_plt.jpg";
 const adAccountId = "1177075676820030"; // PHÚC LỘC THỌ
-
-let oodo_view = false;
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("#dom_account_name").textContent = NAME;
-  document.querySelector("#dom_account_name_r").textContent = NAME;
-  document.querySelector("#dom_account_id").textContent = adAccountId;
-  document.querySelector("#dom_account_src").src = LOGO_ACCOUNT;
-  document.querySelector("#dom_account_src_r").src = LOGO_ACCOUNT;
-  document.title = `${NAME} - Meta Ads Report by DOM Marketing`;
-});
