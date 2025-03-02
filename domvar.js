ac8c7e896548c48e84e175f4c1b77282c5996fb2
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2025-03-02 17:24:34 +0700

    Update domvar.js

--- domvar.js
+++ domvar.js
@@ -1,15 +1,18 @@
-
-const accessToken =
-  "EAAQwS9m6tIABO8ZCZCvO4TtPBXcbilAYn3nwZCZB739B8GtMfy2V2uJmgjHMtvsdKS6XMl7YiBuXqg3BxTdh37H7Vv5qYcsZA7IqVYMLqHX3FhQdxD8fSguISa0sDg1INzOfVtUCt8OoNqh0j6PXvu50rZCgMerGZAJ7NAYgLYuTsPw8NvdOEdF5kRX9C0ctu1ka7CS6VcbbXosWnMM"; // Token của bạn
-const quickFilter = [
-  "Lead Form",
-  "Awareness",
-  "Engagement",
-  "Message",
-  "Traffic",
-  "Pagelike",
+let listAccount = [
+  {
+    brand: false,
+    quick: [
+      "Lead Form",
+      "Awareness",
+      "Engagement",
+      "Message",
+      "Traffic",
+      "Pagelike",
+    ],
+    avatar: "./DOM-img/logo_plt.jpg",
+    name: "Phúc Lộc Thọ",
+    id: "1177075676820030",
+    access:
+      "EAAQwS9m6tIABO8ZCZCvO4TtPBXcbilAYn3nwZCZB739B8GtMfy2V2uJmgjHMtvsdKS6XMl7YiBuXqg3BxTdh37H7Vv5qYcsZA7IqVYMLqHX3FhQdxD8fSguISa0sDg1INzOfVtUCt8OoNqh0j6PXvu50rZCgMerGZAJ7NAYgLYuTsPw8NvdOEdF5kRX9C0ctu1ka7CS6VcbbXosWnMM",
+  },
 ];
-let isBrand = false;
-const accName = "Phúc Lộc Thọ";
-const accAvatar = "./DOM-img/logo_plt.jpg";
-const adAccountId = "1177075676820030"; // PHÚC LỘC THỌ
